15a9b16c4df0280bcc96ae27b4d9f8daf3c814c3
omar10t <mt01051@surrey.ac.uk>
2022-04-17 17:59:44 -0700

    minor styling change for products listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ price: 3105.00)
 Product.create!(title: 'CANON EOS 2000D DSLR Camera with EF-S 18-55 mm f/3.5-5.6 III & EF 75-300 mm f/4-5.6 III Lens ',
 description:
 %{<p>
-<em>Take fantastic photos| Connect your camera|Guided modes| Fast autofocus and processing </em>
+<em>Take fantastic photos | Connect your camera | Guided modes | Fast autofocus and processing </em>
 The Canon EOS 2000D DSLR Camera lets you take photos packed with detail, and Full HD videos that brings action to life. The standard zoom lens included with the camera is perfect for use in a wide range of situations, while the telephoto zoom lens is great for sports and nature.
 </p>},
 image_url: 'pic12.jpg',
