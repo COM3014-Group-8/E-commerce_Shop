b26b389a0a3cb77bf9d72a65cd91ad2fb1318269
omar10t <mt01051@surrey.ac.uk>
2022-04-23 18:51:43 -0700

    Creating Cart, added functionality testing for cart controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_15_015038) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_24_002139) do
   create_table "admin_accounts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -29,6 +29,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_015038) do
   create_table "item_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "payment_checkouts", force: :cascade do |t|
@@ -50,4 +59,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_015038) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
 end
